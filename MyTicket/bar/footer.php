419b79b74089ac1e165f4b19dc949829c88f488f
rafisiregar1444 <147793020+rafisiregar1444@users.noreply.github.com>
2024-12-18 00:03:53 +0700

    Add files via upload

---
+++ MyTicket/bar/footer.php
@@ -0,0 +1,8 @@
+<footer class="app-footer"> <!--begin::To the end-->
+            <div class="float-end d-none d-sm-inline">MyTicket</div> <!--end::To the end--> <!--begin::Copyright--> <strong>
+            Hak Cipta &copy; <?php echo date('Y-m-d'); ?>&nbsp;
+                <a href="#" class="text-decoration-none">TST - D</a>.
+            </strong>
+            Dikembangkan oleh Kelompok.
+            <!--end::Copyright-->
+</footer> <!--end::Footer-->
